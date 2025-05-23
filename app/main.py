ad26e31b06f5641fc52ead4df9430a0fb17348ed
norbertm09 <malu.n@calculus-system.cd>
2025-05-23 16:32:53 +0200

    main.py

--- app/main.py
+++ app/main.py
@@ -1,6 +1,16 @@
 from fastapi import FastAPI
 from routers import payment
+from fastapi.middleware.cors import CORSMiddleware
 
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # ou ["https://votre-site.com"]
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 app = FastAPI(title="API Maître Paiement – Visa & Mastercard")
 
 app.include_router(payment.router, prefix="/pay", tags=["Paiement"])
